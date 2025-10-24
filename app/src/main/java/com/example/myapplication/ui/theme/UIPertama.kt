f03272fbc130ac44ea754e3eba87b2e3cb53caaf
dianfitrip <dianfitri683@gmail.com>
2025-10-24 09:35:31 +0700

    bagian logo atau gambar umy

--- app/src/main/java/com/example/myapplication/ui/theme/UIPertama.kt
+++ app/src/main/java/com/example/myapplication/ui/theme/UIPertama.kt
@@ -30,7 +30,18 @@ fun ActivitasPertama(modifier: Modifier){
             colors = CardDefaults.cardColors(
                 containerColor = Color.DarkGray
             )
-        )
+        ){
+            Row(){
+                val gambar = painterResource(id = R.drawable.logoUMY)
+                image(
+                    painter = gambar,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(size = 100.dp)
+                        .padding(all = 5.dp)
+                )
+            }
+                }
 
     }
 }
